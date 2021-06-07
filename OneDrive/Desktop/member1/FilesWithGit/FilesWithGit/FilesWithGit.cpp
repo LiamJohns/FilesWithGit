d6c714cc103cc727f292d8c5298f4cdf0c2206dc
Liam Johns <liamjohns.rainbow@gmail.com>
2021-06-08 11:01:27 +1200

    my first push to FilesWithGit

---
+++ OneDrive/Desktop/member1/FilesWithGit/FilesWithGit/FilesWithGit.cpp
@@ -0,0 +1,28 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+
+struct shoes {
+
+  int size;
+  string brand;
+  string colour;
+  float price
+
+
+
+};
+
+
+
+int main()
+{
+    
+
+
+
+}
+
